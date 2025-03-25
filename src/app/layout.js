856da7cd7c0fd67f320e5258b692a42c87c16033
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-25 13:28:42 +0100

    style: 💄  add new font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
 // src\app\layout.js
 
 import PropTypes from "prop-types";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "OTT",
@@ -22,8 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ fontFamily: '"Gabarito", sans-serif' }}
+        className="antialiased"
       >
         {children}
       </body>
