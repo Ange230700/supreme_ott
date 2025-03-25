aa7c5425084c12944da61048837ebf8acad3ed37
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-25 16:34:19 +0100

    feat: 🎸  extract component

--- src/app/components/MovieCard.jsx
+++ src/app/components/MovieCard.jsx
@@ -1,9 +1,34 @@
-// src\app\components\MovieCard.jsx
+// src/app/components/MovieCard.jsx
 
 import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import Image from "next/image";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ film }) => {
+  return (
+    <Link href={`/movies/${film.id}`} className="card">
+      <div className="card-pic-frame">
+        <Image
+          src={film.cover_url}
+          alt={film.title}
+          fill
+          sizes="100%, 100%"
+          className="object-cover"
+          priority={true}
+        />
+      </div>
+      <h2 className="card-title">{film.title}</h2>
+    </Link>
+  );
+};
+
+MovieCard.propTypes = {
+  film: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    cover_url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default MovieCard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 // src/app/page.js
 
-import Link from "next/link";
-import Image from "next/image";
+import MovieCard from "./components/MovieCard";
 
 export default async function Home() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -13,19 +12,7 @@ export default async function Home() {
       <h1 className="main-heading">Welcome to OTT Platform</h1>
       <section className="cards-grid">
         {films.map((film) => (
-          <Link key={film.id} href={`/movies/${film.id}`} className="card">
-            <div className="card-pic-frame">
-              <Image
-                src={film.cover_url}
-                alt={film.title}
-                fill
-                sizes="100%, 100%"
-                className="object-cover"
-                priority={true}
-              />
-            </div>
-            <h2 className="card-title">{film.title}</h2>
-          </Link>
+          <MovieCard key={film.id} film={film} />
         ))}
       </section>
     </main>
