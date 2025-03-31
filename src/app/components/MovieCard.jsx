a625d0ba5bc6ee9984666f17c1b60d106285677f
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-31 19:14:15 +0200

    feat: 🎸  blur non-available movies

--- src/app/components/MovieCard.jsx
+++ src/app/components/MovieCard.jsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 const MovieCard = ({ film }) => {
+  const blurClass = film.IsAvailable ? "" : "filter blur-xs";
   return (
     <Link href={`/movies/${film.id}`} className="movie-link">
-      <div className="movie-slide">
+      <div className={`movie-slide ${blurClass}`}>
         <Image
           src={film.miniature_url}
           alt={film.title}
@@ -26,6 +27,7 @@ MovieCard.propTypes = {
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     miniature_url: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    IsAvailable: PropTypes.bool,
   }).isRequired,
 };
 
