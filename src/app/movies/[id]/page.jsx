085d3b37563786525a6443b5362dd21fac68a612
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-02 13:57:21 +0200

    feat: 🎸  make navigation to movie details page possible

--- src/app/movies/[id]/page.jsx
+++ src/app/movies/[id]/page.jsx
@@ -1,1 +1,18 @@
 // src\app\movies\[id]\page.jsx
+
+import PropTypes from "prop-types";
+
+export default async function MovieDetail({ params }) {
+    const { id } = await params;
+
+    return (
+        <div>
+            <h1>Movie Detail Page</h1>
+            <p>You are viewing movie with ID: {id}</p>
+        </div>
+    );
+}
+
+MovieDetail.propTypes = {
+    params: PropTypes.object.isRequired,
+};
