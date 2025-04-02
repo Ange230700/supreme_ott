ecd9d1aa77f7af01db233bc120e45b60060de2ca
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-02 16:43:54 +0200

    feat: 🎸  implement search page

--- src/app/pages/search/page.jsx
+++ src/app/pages/search/page.jsx
@@ -2,8 +2,44 @@
 
 "use client";
 
+import { useState } from "react";
+import { useMovies } from "@/app/hooks/useMovies";
+import MovieCard from "@/app/components/home/movieCarousel/MovieCard";
+
 export default function Search() {
+    const [searchValue, setSearchValue] = useState("");
+    const { movies } = useMovies();
+
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
     return (
-        <section className="search-page">Search page</section>
-    )
+        <section className="search-page">
+            <div className="search-display-section">
+                <div className="search-bar-container">
+                    <input
+                        className="search-bar"
+                        type="search"
+                        placeholder="Search for a movie"
+                        value={searchValue}
+                        onChange={handleSearchChange}
+                    />
+                </div>
+                <div className="search-result-container">
+                    {searchValue.length > 0
+                        ? movies
+                            .filter((movie) =>
+                                movie.title.toLowerCase().includes(searchValue.toLowerCase())
+                            )
+                            .map((movie) => (
+                                <MovieCard key={movie.id} film={movie} />
+                            ))
+                        : movies.map((movie) => (
+                            <MovieCard key={movie.id} film={movie} />
+                        ))}
+                </div>
+            </div>
+        </section>
+    );
 }
